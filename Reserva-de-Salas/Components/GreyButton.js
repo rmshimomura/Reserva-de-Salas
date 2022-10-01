8a7446d3fbe012b4ed4b8314b009fdf5e7934cf1
Matheus Vila Real <mpvr0409@gmail.com>
2022-10-01 17:22:07 -0300

    Botões verde e cinza prontos

--- Reserva-de-Salas/Components/GreyButton.js
+++ Reserva-de-Salas/Components/GreyButton.js
@@ -3,31 +3,31 @@ import { StyleSheet } from "react-native"
 
 import './CustomButton'
 
-const BACKGROUND = '#00FF00'
+const BACKGROUND = '#333333'
 const FONT_SIZE = 20
 const FONT_COLOR = '#FFFFFF'
 const FONT_WEIGHT = 'bold'
 
-const GreenButton = ({text, onPress, style, textStyle}) => (
+const GreyButton = ({text, onPress, style, textStyle}) => (
 
     <CustomButton text={text} 
         onPress={onPress} 
-        buttonStyle={[style, styles.greenButtonStyle]} 
+        buttonStyle={[style, styles.greyButtonStyle]} 
         textStyle={textStyle} />
 )
 
 const styles = StyleSheet.create({
 
-    greenButtonStyle: {
+    greyButtonStyle: {
         backgroundColor: BACKGROUND,
         borderRadius: 10,
         padding: 10
     },
-    greenButtonTextStyle: {
+    greyButtonTextStyle: {
         fontSize: FONT_SIZE,
         color: FONT_COLOR,
         fontWeight: FONT_WEIGHT
     }
 })
 
-export default GreenButton
+export default GreyButton
