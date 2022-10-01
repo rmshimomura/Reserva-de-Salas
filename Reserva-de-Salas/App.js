1c70397ff4e4b522a7a0d8a8eb7e0ce02c648a58
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-10-01 18:01:44 -0300

    foi

--- Reserva-de-Salas/App.js
+++ Reserva-de-Salas/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Login from './Components/Menus/Login';
-import MenuAdministrador from './Components/Menus/MenuAdministrador';
+import MenuSecretaria from './Components/Menus/MenuSecretaria';
 
 const Stack = createStackNavigator()
 
@@ -12,12 +12,30 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Login" options={{headerShown: false}}>
-          {props => <MenuAdministrador {...props} />}
+          {props => <Login {...props} />}
         </Stack.Screen>
-
+        <Stack.Screen name="Menu secretaria" options={{headerShown: false}}>
+          {props => <MenuSecretaria {...props} />}
+        </Stack.Screen>
+        {/* <Stack.Screen name="Menu professor">
+          {props => <MenuProfessor {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu aluno">
+          {props => <MenuAluno {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu coordenador">
+          {props => <MenuCoordenador {...props} />}
+        </Stack.Screen> */}
       </Stack.Navigator>
-    </NavigationContainer>
+  </NavigationContainer>
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
