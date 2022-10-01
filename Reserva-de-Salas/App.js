ea8d87cb69436aeef06ee4f4d8e3a27d4f64575d
RhuanFerrer <rhuanferrerr@gmail.com>
2022-10-01 19:43:10 -0300

    sync

--- Reserva-de-Salas/App.js
+++ Reserva-de-Salas/App.js
@@ -4,6 +4,10 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { NavigationContainer } from '@react-navigation/native'
 import Login from './Components/Menus/Login';
 import MenuSecretaria from './Components/Menus/MenuSecretaria';
+import MenuProfessor from './Components/Menus/MenuProfessor';
+import MenuAluno from './Components/Menus/MenuAluno';
+import MenuCoordenacao from './Components/Menus/MenuCoordenacao';
+import MenuAdministrador from './Components/Menus/MenuAdministrador';
 
 const Stack = createStackNavigator()
 
@@ -16,6 +20,18 @@ export default function App() {
         </Stack.Screen>
         <Stack.Screen name="Menu secretaria" options={{headerShown: false}}>
           {props => <MenuSecretaria {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu professor" options={{headerShown: false}}>
+          {props => <MenuProfessor {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu aluno" options={{headerShown: false}}>
+          {props => <MenuAluno {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu coordenador" options={{headerShown: false}}>
+          {props => <MenuCoordenacao {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Menu admin" options={{headerShown: false}}>
+          {props => <MenuAdministrador {...props} />}
         </Stack.Screen>
       </Stack.Navigator>
   </NavigationContainer>
