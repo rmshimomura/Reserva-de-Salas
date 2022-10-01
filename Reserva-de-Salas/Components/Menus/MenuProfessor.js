dbdcc7d016d7941769caeda171b48268c510e301
Enzo Guido Americano da Costa <enzoguido.a375@uel.br>
2022-10-01 17:56:08 -0300

    Modelo base menu professor

---
+++ Reserva-de-Salas/Components/Menus/MenuProfessor.js
@@ -0,0 +1,30 @@
+import react from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function MenuAluno(props) {
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Menu Professor</Text>
+            <Button title="Consultar Sala" onPress={this.props.navigation.navigate('Consultar Sala')} />
+            <Button title="Fazer Requisição" onPress={this.props.navigation.navigate('Fazer Requisição')} />
+            <Button title="Minhas Requisições" onPress={this.props.navigation.navigate('Minhas Requisições')} />
+            <Button title="Libear Sala" onPress={this.props.navigation.navigate('Libear Sala')} />
+            <Button title="Sair" onPress={() => props.navigation.navigate('Login')} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+});
