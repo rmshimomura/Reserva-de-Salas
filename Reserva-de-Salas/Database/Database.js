0c917cc68240934d44f8f3bbe7d238c33846c083
felipe based mito <42080804+felipeyousoro@users.noreply.github.com>
2022-10-02 20:03:12 -0300

    cadastrar sala feito

--- Reserva-de-Salas/Database/Database.js
+++ Reserva-de-Salas/Database/Database.js
@@ -5,8 +5,13 @@ export class DataBase {
     
     static classroomsDB = require('./Classroom/Classrooms.json'); 
     static loginDB = require('./Login/Users.json'); 
+    static deptDB = require('./Department/Department.json'); 
     
     constructor(){}
+
+    static searchDepto(center) {
+        return this.deptDB.centros.find((centr) => centr.centro === center);
+    }
 
     static searchUsername(name){
         return this.loginDB.users.find((user) => user.name === name);
